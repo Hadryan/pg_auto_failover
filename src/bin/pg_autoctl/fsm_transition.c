6f4993a287caa6f130bacac0953543807dc110ae
Dimitri Fontaine <dimitri@citusdata.com>
2019-12-24 20:02:25 +0100

    Implement updating synchronous_standby_names.
    When the replication properties of some nodes change, we should update the
    primary's setting of synchronous_standby_names. We do that by assigning
    either WAIT_PRIMARY or JOIN_PRIMARY to the primary node as soon as someone
    changes its replication properties.

--- src/bin/pg_autoctl/fsm_transition.c
+++ src/bin/pg_autoctl/fsm_transition.c
@@ -867,7 +867,6 @@ bool
 fsm_promote_standby(Keeper *keeper)
 {
 	LocalPostgresServer *postgres = &(keeper->postgres);
-	bool otherNodeMissingIsOk = true;
 
 	if (!ensure_local_postgres_is_running(postgres))
 	{
